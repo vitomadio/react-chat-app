c3b8dab2d3cfc23dafc12d499d6b7898c4b31498
vito madio <vitomadio@MacBook-Pro-de-vito.local>
2020-08-29 16:12:48 +0200

    Updates

---
+++ precache-manifest.e11883435ded94a602eba430650d3f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71380c2321f7a14f26d0bb3ca87bf611",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "a524191df895f1e4cde0",
+    "url": "/react-chat-app/static/css/main.45250696.chunk.css"
+  },
+  {
+    "revision": "365d2d652aed11589262",
+    "url": "/react-chat-app/static/js/2.5b0d218f.chunk.js"
+  },
+  {
+    "revision": "f48955458710786629c9fab78abba7c6",
+    "url": "/react-chat-app/static/js/2.5b0d218f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a524191df895f1e4cde0",
+    "url": "/react-chat-app/static/js/main.ecc611bc.chunk.js"
+  },
+  {
+    "revision": "efd3442585b4b34ee6c8",
+    "url": "/react-chat-app/static/js/runtime-main.1aae6afd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.24c65905b418deb59b90c5b0df6082d2.js"
+  "/react-chat-app/precache-manifest.e11883435ded94a602eba430650d3f13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-chat-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
