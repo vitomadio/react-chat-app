62218259973a98abf8e8fe8bb5a6972c02d1d772
vito madio <vitomadio@MacBook-Pro-de-vito.local>
2020-08-29 15:33:28 +0200

    Updates

---
+++ precache-manifest.083a27685a7fe88b759e28a57585d4ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4194ab013380ec44cd1c97983f606c",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "73baef17ec1485159657",
+    "url": "/react-chat-app/static/css/main.45250696.chunk.css"
+  },
+  {
+    "revision": "365d2d652aed11589262",
+    "url": "/react-chat-app/static/js/2.5b0d218f.chunk.js"
+  },
+  {
+    "revision": "f48955458710786629c9fab78abba7c6",
+    "url": "/react-chat-app/static/js/2.5b0d218f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73baef17ec1485159657",
+    "url": "/react-chat-app/static/js/main.4b1d1608.chunk.js"
+  },
+  {
+    "revision": "efd3442585b4b34ee6c8",
+    "url": "/react-chat-app/static/js/runtime-main.1aae6afd.js"
+  }
+]);
